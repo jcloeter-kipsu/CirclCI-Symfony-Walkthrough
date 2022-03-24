73ba42398d22cec4f09f9167f9c1088323821d8f
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-03-24 14:13:09 -0500

    added entity and tests

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomerController extends AbstractController
+{
+    #[Route('/', name: 'customer', methods: "GET")]
+    public function index(): Response
+    {
+        $customerName="John Doe";
+        return $this->render('customer/index.html.twig', [
+            'name' => $customerName
+        ]);
+    }
+}
---
+++ tests/Entity/CustomerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Customer;
+use PHPUnit\Framework\TestCase;
+
+class CustomerTest extends TestCase
+{
+
+    public function testSettingCustomerFirstName()
+    {
+        $customer = new Customer();
+        $firstName='Jimbo';
+
+        $customer->setFirstName($firstName);
+
+        $this->assertEquals($firstName, $customer->getFirstName());
+    }
+
+    public function testSettingCustomerLastName()
+    {
+        $customer= new Customer();
+
+        $lastName='Neutron';
+        $customer->setLastName($lastName);
+
+        $this->assertEquals($lastName, $customer->getLastName());
+    }
+
+    public function testReturnsCustomerFullName()
+    {
+        $customer = new Customer();
+        $customer->setFirstName("John");
+        $customer->setLastName("Deo");
+
+        $fullName = $customer->getFirstName() . ' ' . $customer->getLastName();
+
+        $this->assertSame($fullName, $customer->getCustomerFullName());
+    }
+}
